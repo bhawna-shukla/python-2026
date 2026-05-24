15668ab18bdb4bcabd52b823ee1274610e4cb5f2
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-24 12:13:27 +0530

    first commit

---
+++ day-01/datatype.py
@@ -0,0 +1,11 @@
+name = "bhawna shukla"
+age = 24
+price = 100.50
+
+print(name)
+print(age)
+print(price)
+
+print(type(name))
+print(type(age))
+print(type(price))
---
+++ day-01/index.py
@@ -0,0 +1,20 @@
+# python is a developed by guido van rossum in 1991. It is a high level programming language. 
+""" It is used for web development, data analysis, artificial intelligence, scientific computing and many more. 
+It is a popular language among developers and data scientists.
+It is easy to learn and has a large community of developers who contribute to its development and support.
+portable
+easy  to learn
+free and open source"""
+
+
+
+
+
+
+print("Welcome to my Github page")
+print("My name is Bhawna shukla")
+print("I am learning python for datat analysis")
+
+
+
+
