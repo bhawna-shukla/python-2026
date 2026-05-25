6771b8bc52187b22e8bc0135119b7eafc7192857
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-25 13:49:57 +0530

    Basic _10Question

--- day-01/index.py
+++ day-01/index.py
@@ -1,6 +1,5 @@
 # python is a developed by guido van rossum in 1991. It is a high level programming language. 
-""" It is used for web development, data analysis, artificial intelligence, scientific computing and many more. 
-It is a popular language among developers and data scientists.
+""" It is used for web development, data analysis, artificial intelligence, scientific computingand many more. It is a popular language among developers and data scientists.
 It is easy to learn and has a large community of developers who contribute to its development and support.
 portable
 easy  to learn
@@ -11,9 +10,17 @@ free and open source"""
 
 
 
+
 print("Welcome to my Github page")
 print("My name is Bhawna shukla")
 print("I am learning python for datat analysis")
+
+
+
+name = input("enter your name: ")
+print("welcome to python programming", name)
+
+
 
 
 
---
+++ day-01/practiceQ.py
@@ -0,0 +1,81 @@
+print("---------------------------------------")
+print(" Q1: Program to Subtract Two Integers") 
+
+num1 = 300
+num2 = 20
+sub = num1 - num2
+print("The subtraction of two numbers is :", sub)
+print("---------------------------------------")
+print("2. Program to Multiply Two Float Numbers")
+num3 = 10.5
+num4 = 20.5
+mul = num3 * num4
+print("The multiplication of two numbers is :", mul)
+print("---------------------------------------")
+print("3. Program to Divide Two Numbers")
+a = 4567
+b = 324
+div = a/b
+print("The division of two numbers is :", div)
+print("---------------------------------------")
+print("4. Program to Find Remainder Using Modulus Operator")
+x= 10
+y = 3
+mod = x % y
+print("The modulus of two numbers is :", mod)
+print("---------------------------------------")
+print("5. Program to Calculate Simple Interest")
+principal = 1000
+rate = 5
+time = 2
+simple_interest = (principal * rate * time) / 100
+print("The simple interest is :", simple_interest)
+print("---------------------------------------")
+print("6. Program to Swap Two Numbers")
+m = 40
+n = 30
+print("before swaping ")
+print("m =", m)
+print("n =", n)
+temp = m
+m = n
+n = temp
+print("after swaping ")
+print("m =", m)
+print("n =", n)
+print("---------------------------------------")
+print("7. Program to Calculate Average of Three Numbers")
+avg1 =10
+avg2 = 40
+avg3 = 30
+print("first number is:" , avg1)
+print("second number is:" , avg2)
+print("third number is:" , avg3)
+average = (avg1+ avg2 + avg3) / 3
+print("The average of three numbers is :", average)
+print("---------------------------------------")    
+print("8. Program to Convert Float into Integer")
+con = 10.6
+print("before conversion:", con )
+con = int(con)
+print("after conversion:", con)
+print("---------------------------------------")
+print("9. Program to Check the Data Type of Different Variables")
+var1 = "Hello, World!" 
+var2 = 42
+var3 = 3.14
+print("Data type of var1:", type(var1))
+print("Data type of var2:", type(var2))
+print("Data type of var3:", type(var3))
+print("---------------------------------------")
+
+print("10. input the Program to Print your Name, Age, and Course")
+name = input("enter your name: ")
+age = input("enter your age: ")
+course = input("enter your course: ")
+print("Your Name is :", name)
+print("Your Age is :", age)
+print("Your Course is :", course)
+print("Thank you for providing your details")
+print("---------------------------------------")
+
