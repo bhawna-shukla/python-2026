cef62b7a6c5386c5543a5c74da9adfb72490abb0
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-26 11:33:02 +0530

    concatenation_string

---
+++ day-02/concatenation.py
@@ -0,0 +1,10 @@
+str1 = "python programming"
+str2 = "language"
+print(str1+ " "+str2)
+
+
+print("-------------------------------")
+str3 = "python"
+str4 = "programming"
+conc = str3 + " " + str4
+print(conc)
