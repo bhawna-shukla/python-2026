de1750bd5f37df84828e76693e56e9be547ae512
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-26 11:49:22 +0530

    length_string

---
+++ day-02/length.py
@@ -0,0 +1,9 @@
+str1 = "python"
+print(len(str1))
+print("-------------------------------")
+str2 = "python programming"
+print(len(str2))
+
+print("-------------------------------")
+str3 = "python programming" + " " + "language"
+print(len(str3))
