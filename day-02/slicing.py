ffe4bac15da0d825dddabd3b5741960dc1b0bf08
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-26 12:22:32 +0530

    slicing_string

---
+++ day-02/slicing.py
@@ -0,0 +1,17 @@
+str1 ="Bhawna Shukla"
+print(str1[0:5])
+print("-------------------------------")
+ 
+str2 = "BHAWNA SHUKLA"
+print(str2[:5])
+print("-------------------------")
+print(str1[8 : ])
+
+
+# negative 
+print("-------------------------")
+print(str1[-8 : -3])
+print("-------------------------")
+print(str1[-8 : ])
+print("-------------------------")
+print(str1[ : -3])
