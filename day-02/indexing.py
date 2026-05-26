0d9da87aa39d93bdd2d487edbbad0f333cfcba9d
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-26 11:54:15 +0530

    indexing_string

---
+++ day-02/indexing.py
@@ -0,0 +1,12 @@
+str1 = "apna_college"
+print(str1[6])
+
+print("-------------------------------")
+print(str1[4])
+
+print("-------------------------------")
+
+str2 = "python   programming"
+print(str2[16])
+
+
