b54586cc8320fdbfc800b8f824e101fb9a66ca62
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-26 12:53:51 +0530

    func_string and some ques

---
+++ day-02/string_function.py
@@ -0,0 +1,43 @@
+#returns true if string ends with substr (endswith())
+str = "apna school"
+print(str.endswith("hool"))
+print(str.endswith("hoop"))
+
+print("--------------------------")
+#capitalize() capitalize  1 char
+str1 = str.capitalize()
+print(str1)
+
+print("--------------------------")
+#replace(old.new) replaces all occurrences of old with new
+str3 = str.replace("a" ,"o")
+print(str3)
+
+print("--------------------------")
+#find(sub) returns the index of first occurrence of sub in string. If sub is not found, it returns -1
+str4 = str.find("n")
+print(str4)
+
+print("--------------------------")
+#count(sub) returns the number of occurrences of sub in string
+str5 = str.count("o")
+print(str5) 
+
+
+
+
+
+
+
+
+#WAP to the occurence  of $ in a string 
+val = " $1000 is the price of this product. I have $500 in my wallet."
+val1 = val.count("$")
+print(val1)
+
+#WAP to input user's  first name & print its  lenght
+name = input ("Enter your name 1:")
+name1 = len(name)
+print(name1)
+
+
