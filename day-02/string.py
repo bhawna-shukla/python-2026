cad263924e9343498c0d4001627e90aca30dd846
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-26 11:26:08 +0530

    string.py

---
+++ day-02/string.py
@@ -0,0 +1,18 @@
+#what is string ?
+#string is a data types that stores a sequence of character 
+
+
+str1 ="hello world "
+str2 = 'python programming '
+str3 = """this is a multi line string it  can span multiple lines like this"""
+print(str1)
+print(str2)
+print(str3)
+
+print("----------------------------------------")
+#escap character in string
+str4 = "i am a \npython  developer"
+str5 = 'i am a \t python developer'
+
+print(str4)
+print(str5)
