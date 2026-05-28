2de5670aa355d15e3ecce11df980f89b0051235f
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-28 15:51:59 +0530

    list.......

---
+++ day-04/list.py
@@ -0,0 +1,14 @@
+# In Python, a list is a built-in data type used to store multiple items in a single variable.
+#A list is an ordered collection of items that is mutable (you can change it anytime).
+
+marks=[90, 85, 78, 92, 88]
+print(marks)
+
+print("--------------------")
+#list is 
+student = [ ' ram', 90.5 , 16 , 'delhi']
+print(student)
+print("after change the name")
+student[0] = 'shyam'
+print(student)
+print("--------------------")
