2d26236e6825bbf8dcdef458e83d2d0f14ad1fe9
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-28 17:20:58 +0530

    tuple.py

---
+++ day-04/tuple.py
@@ -0,0 +1,12 @@
+#In Python, a tuple is a built-in data type used to store multiple items in a single variable.
+tup = (2,3,4,5,5,4,4)
+print(tup)
+print("--------------------")
+print("----------------------")
+#methods
+#return index of first occurrence 
+print(tup.index(4))
+print("---------------------------------")
+print(tup.count(4))
+# practice questions
+print("----------------------------")
