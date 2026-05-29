4b5d7147048b1559311d3b7360cfcaacee6a0817
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-29 14:04:18 +0530

    nested

---
+++ day-05/nested_dic.py
@@ -0,0 +1,12 @@
+#data of student
+student ={
+    "name" : "rahul",
+    "subject" : {
+    "math" : 78,
+    "phy" : 58,
+    "chem" : 89,
+    "eng" : 99
+    }
+}
+print(student)
+print("mark is: ",student["subject"]["math"])
