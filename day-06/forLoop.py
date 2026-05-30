18a3c932a9888332ec54f1b38e4c7da9d35e1830
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-30 16:13:52 +0530

    for loop

---
+++ day-06/forLoop.py
@@ -0,0 +1,35 @@
+#A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+num = [1,3,2,4,5,6,7]
+
+for val in num :
+    print(val)
+
+print("---------------------------")
+color = ("red","blue","green","black")
+for a in color:
+ print(a)
+
+ 
+ 
+print("----------------------------]")
+
+list = [1,4,9,16,25,36,49,64,81,100]
+for nums in list :
+   print(nums)
+
+print("---------------------------")
+
+tup = (1,4,9,16,25,36,49,64,81,49,100)
+x = 49
+
+idx = 0
+for m in tup:
+    if m == x :
+      print("number found at idx :", idx)
+    idx += 1
+
+print("----------------------")
+
+     
+   
+
