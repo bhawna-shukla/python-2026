1e264728e9a910d6bd47089e082d3eb6d45fc687
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-05-31 14:12:04 +0530

    strat function

---
+++ day-06/pattern.py
@@ -0,0 +1,18 @@
+num = int(input("enter the number : "))
+
+for i in range(0,num+1):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+print("-----------------------")
+#odd number start print 
+a = int(input("enter the number : "))
+k= 2
+for i in range(1,a+1):
+    for j in range(2,k+1):
+        print("*",end=" ")
+        
+    print()
+    print()
+
---
+++ day-07/func.py
@@ -0,0 +1,32 @@
+#function definition
+
+def cal_sum(a,b): #parameters
+    sum = a+b 
+    print(sum)
+    return sum
+
+cal_sum(5,6)   #function call   ;   arguments
+
+cal_sum(678,89)
+#simple cod
+
+def cal_sum(a,b):
+    return a + b
+
+sum = cal_sum(1,2)
+print(sum)
+
+
+#print hello 
+def print_hello():
+    print("helllo")
+
+print_hello()
+#average of three number 
+def avg_num(a,b,c):
+    sum = a+b+c
+    avg = sum/3
+    print(avg)
+    return avg
+ 
+avg_num(2,4,6)
