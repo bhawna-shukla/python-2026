c29e1bd55d54d2b0bd0abec389c354ecb05b3391
Bhawna Shukla <shuklabhawna81@gmail.com>
2026-06-01 13:47:26 +0530

    practice qs

--- day-07/practice.py
+++ day-07/practice.py
@@ -17,11 +17,35 @@ def print_el(list):
 print_el(cities)
 print()
 
+#WAP to find the factorial of n ( n is parameter)
+
+def cal_fact(n):
+    fact = 1
+    for val in range(1, n+1):
+        fact *= val
+    print(fact)
+
+cal_fact(6) 
+
+#WAF to convert  USD to INR
+
+def convert(USD):
+    inr = USD * 94.84
+    print(USD,"USD=",inr , "inr")
+
+convert(7)
 
 
+#HOME WORK QS 
 
 
-
+def cal_math(num):
+    if(num%2==0):
+        print(num,"is a even number")
+    else:
+        print(num,"is a odd number")
+        
+num=cal_math(3)
 
 
 
